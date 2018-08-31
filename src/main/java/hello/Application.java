d13f58fc5b13185a3cfe8484c13681940c4308c2
Adailson Aguiar <adailsonacj@live.com>
2018-08-31 08:26:55 -0300

    Adição do pacote de repositórios além do pacote de modelos

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,13 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EntityScan(basePackages = {
-        "model"
-})
-@EnableJpaRepositories(basePackages = {
-        "repository"
-})
 public class Application {
 
     public static void main(String[] args) {
